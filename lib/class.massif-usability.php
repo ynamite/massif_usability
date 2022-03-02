fdc905c534cb41eaf56bc9015b47f95ce8be5174
ynamite <ynamite@bluewin.ch>
2022-03-02 18:47:05 +0100

    add custom functionality

--- lib/class.massif-usability.php
+++ lib/class.massif-usability.php
@@ -12,9 +12,73 @@ class massif_usability
         $table     = $ep->getParam('table');
         $tableName = $table->getTableName();
 
+        $addNameColLink = ['rex_yf_color' => ['name'], 'rex_yf_tag' => ['name'], 'rex_yf_event' => ['headliner'], 'rex_yf_gallery' => ['title'], 'rex_yf_login' => ['username'], 'rex_yf_menu' => ['text_standard'], 'rex_yf_news' => ['headliner'], 'rex_yf_time' => ['title']];
+        $showImgInListForTable = ['rex_yf_event' => 'image', 'rex_yf_activity' => 'image', 'rex_yf_gallery' => 'image'];
+        $noIdYformTables = ['rex_yf_color', 'rex_yf_gallery', 'rex_yf_tag', 'rex_yf_login', 'rex_yf_menu', 'rex_yf_news', 'rex_yf_time'];
+        $checkboxesYformTables = [];
+
+        $_csrf_key = \rex_yform_manager_table::get($tableName)->getCSRFKey();
+        $_csrf_params = \rex_csrf_token::factory($_csrf_key)->getUrlParams();
+
+        /*
+        *   yform 4 status switch and dupblicate fix, needed until yform_usability is updated
+        */
+
+        if (in_array($tableName, $noIdYformTables)) {
+            $list->removeColumn("id");
+        };
+
+        /*if($tableName == 'rex_yf_projekte') {
+            $list = $ep->getSubject();
+            $f = 'baujahr';
+            $list->setColumnFormat($f, 'custom', function ($params) use($f) {
+                return date('Y', strtotime($params['list']->getValue($f)));
+            });
+        }*/
+
+
+        foreach ($checkboxesYformTables as $table => $field) {
+            if ($tableName == $table) {
+                $field = is_array($field) ? $field : [$field];
+                foreach ($field as $f) {
+                    $list->setColumnFormat($f, 'custom', function ($params) use ($f) {
+                        return ($params['list']->getValue($f) == 1) ? '<i class="fa fa-check"></i>' : '';
+                    });
+                }
+            }
+        }
+
+
+        foreach ($showImgInListForTable as $table => $field) {
+            if ($tableName == $table) {
+                $list->setColumnParams($field, ['func' => 'edit', 'data_id' => '###id###', 'table' => $table, \rex_csrf_token::PARAM => $_csrf_params]);
+                $list->setColumnFormat($field, 'custom', function ($params) use ($field) {
+                    if ($params['list']->getValue($field))
+                        return $params['list']->getColumnLink($field, '<img src="index.php?rex_media_type=rex_media_small&rex_media_file=' . $params['list']->getValue($field) . '" class="thumbnail" style="margin-bottom: 0" />');
+                });
+            }
+        }
+
+
+        foreach ($addNameColLink as $table => $field) {
+            if ($tableName == $table) {
+
+                $field = is_array($field) ? $field : [$field];
+                foreach ($field as $f) {
+                    $list->setColumnParams($f, ['func' => 'edit', 'data_id' => '###id###', 'table' => $table, \rex_csrf_token::PARAM => $_csrf_params]);
+                    $list->setColumnFormat($f, 'custom', function ($params) use ($f) {
+                        return $params['list']->getColumnLink($f, $params['list']->getValue($f));
+                    });
+                }
+            }
+        }
+
+        //$list = self::addDuplicationTrigger($list, $tableName);
         $list = self::addStatusToggle($list, $tableName);
         $list->setColumnPosition(\rex_i18n::msg('yform_function') . ' ', -1);
         $list = self::addCustomToggles($list, $tableName);
+
+
 
         $ep->setSubject($list);
     }
